58f4446247b477ca9ffb057f81938569e5fdf870
Lefcott <leandro.cotti@widergy.com>
2020-12-18 22:46:51 -0300

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -2,9 +2,9 @@ import { runExec, project } from './utils';
 
 (async () => {
   await runExec(`cd ${project.path}; next build`);
-  await runExec(`mv ${project.path}/.next src`);
+  await runExec(`mv ${project.path}/.next .`);
   console.log('build finished!');
-  // await runExec('cp .next/routes-manifest.json .');
-  await runExec('cd src; pwd');
-  await runExec('cd src; ls -la');
+  await runExec('cp .next/routes-manifest.json .');
+  await runExec('pwd');
+  await runExec('ls -la');
 })();
