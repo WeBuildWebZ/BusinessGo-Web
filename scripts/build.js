6460d517d58005a52cfe748c68fe9ed58fe9de71
Lefcott <leandro.cotti@widergy.com>
2020-12-18 22:37:21 -0300

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ runExec(`cd ${project.path}; next build`).then(() => {
   runExec(`mv ${project.path}/.next .`).then(() => {
     setTimeout(() => {
       console.log('build finished!');
-      runExec('locate routes-manifest.json"');
+      runExec('locate routes-manifest.json');
     }, 5000);
   });
 });
