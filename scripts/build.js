9ca361bb55c456469d77f191b4a5e646020b6840
Lefcott <leandro.cotti@widergy.com>
2020-12-18 22:49:54 -0300

    adjust

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,4 @@ import { runExec, project } from './utils';
 (async () => {
   await runExec(`cd ${project.path}; next build`);
   await runExec(`mv ${project.path}/.next .`);
-  console.log('build finished!');
-  await runExec('cp .next/routes-manifest.json .');
-  await runExec('pwd');
-  await runExec('ls -la');
 })();
