6382bc49f2ee03d6bb19a144772154bd33c2d77f
Lefcott <leandro.cotti@widergy.com>
2020-12-18 22:34:01 -0300

    add build message

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,9 @@
 import { runExec, project } from './utils';
 
 runExec(`cd ${project.path}; next build`).then(() => {
-  runExec(`mv ${project.path}/.next .`);
+  runExec(`mv ${project.path}/.next .`).then(() => {
+    setTimeout(() => {
+      console.log('build finished!');
+    }, 5000);
+  });
 });
