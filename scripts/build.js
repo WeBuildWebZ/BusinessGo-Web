8388fd14cf342ec6d45783e84dbf59871de68b3c
Lefcott <leandro.cotti@widergy.com>
2020-12-18 22:35:23 -0300

    add build message

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,7 @@ runExec(`cd ${project.path}; next build`).then(() => {
   runExec(`mv ${project.path}/.next .`).then(() => {
     setTimeout(() => {
       console.log('build finished!');
+      runExec('locate routes-manifest.json"');
     }, 5000);
   });
 });
