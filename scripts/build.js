d4eeeab96ab78385d438b491476fb7c66f2184a0
Lefcott <leandro.cotti@widergy.com>
2020-12-20 23:26:12 -0300

    adjust public folder

--- scripts/build.js
+++ scripts/build.js
@@ -4,4 +4,6 @@ import { runExec, project } from './utils';
   await runExec(`cd ${project.path}; next build`);
   await runExec('rm -rf .next');
   await runExec(`mv ${project.path}/.next .`);
+  await runExec('rm -rf public');
+  await runExec(`cp ${project.path}/public .`);
 })();
