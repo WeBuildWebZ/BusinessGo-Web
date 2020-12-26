84071cb87d9601cf59ac21237161ab4fb4e04b9a
Lefcott <leandro.cotti@widergy.com>
2020-12-26 13:35:14 -0300

    included /admin

---
+++ src/components/Configuration/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Configuration = () => <div>Configuration</div>;
+
+export default Configuration;
---
+++ src/projects/psa/pages/admin/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Configuration from '../../../../components/Configuration';
+
+const PSAAdmin = () => <Configuration />;
+
+export default PSAAdmin;
