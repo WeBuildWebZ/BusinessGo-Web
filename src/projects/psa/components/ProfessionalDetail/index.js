36d301e44dac66a78bdec91741f43ff8d8aabb2e
Lefcott <leandro.cotti@widergy.com>
2020-12-31 18:37:58 -0300

    block scroll on professional detail

--- src/projects/psa/components/ProfessionalDetail/index.js
+++ src/projects/psa/components/ProfessionalDetail/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import './style.css';
 import Content from './components/content';
 
-const ProfessionalDetail = props => (
-  <>
-    <div className="ProfessionalDetail-overlay" onClick={props.onClose} />
-    <div className="ProfessionalDetail">
-      <Content professional={props.professional} onClose={props.onClose} />
-    </div>
-  </>
-);
+const ProfessionalDetail = props => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  });
+
+  return (
+    <>
+      <div className="ProfessionalDetail-overlay" onClick={props.onClose} />
+      <div className="ProfessionalDetail">
+        <Content professional={props.professional} onClose={props.onClose} />
+      </div>
+    </>
+  );
+};
 
 ProfessionalDetail.propTypes = {
   professional: PropTypes.object.isRequired,
--- src/projects/psa/components/Professionals/index.js
+++ src/projects/psa/components/Professionals/index.js
@@ -1,24 +1,18 @@
-import React from "react";
-import { Spinner } from "react-bootstrap";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-import "./style.css";
-import Card from "./components/card";
+import './style.css';
+import Card from './components/card';
 
-const Professionals = (props) => {
+const Professionals = props => {
   const { professionals, loading } = props;
 
   return (
     <div className="Professionals">
-      {loading && (
-        <Spinner animation="grow" className="Professionals-spinner" />
-      )}
+      {loading && <Spinner animation="grow" className="Professionals-spinner" />}
       {professionals.map((professional, i) => (
-        <Card
-          key={i}
-          professional={professional}
-          onClick={() => props.onClick(professional)}
-        />
+        <Card key={i} professional={professional} onClick={() => props.onClick(professional)} />
       ))}
     </div>
   );
@@ -27,11 +21,11 @@ const Professionals = (props) => {
 Professionals.propTypes = {
   professionals: PropTypes.arrayOf(PropTypes.object).isRequired,
   loading: PropTypes.bool.isRequired,
-  onClick: PropTypes.func,
+  onClick: PropTypes.func
 };
 
 Professionals.defaultProps = {
-  onClick: () => {},
+  onClick: () => {}
 };
 
 export default Professionals;
