44d72c9802e59ac0dd11f13b26768d294ef00e76
Lefcott <leandro.cotti@widergy.com>
2021-01-04 00:48:02 -0300

    fix font

--- next.config.js
+++ next.config.js
@@ -78,14 +78,6 @@ module.exports = {
       ]
     });
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/
-      },
-      use: ['@svgr/webpack']
-    });
-
     return config;
   },
   env: { ...env },
