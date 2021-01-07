d0a4f4b4013d112b800a9d5fe72c346ac58c8624
Lefcott <leandro.cotti@widergy.com>
2021-01-07 13:34:10 -0300

    better_requests

--- src/services/session.js
+++ src/services/session.js
@@ -1,20 +1,7 @@
-import axios from '../utils/axios';
+import api from '../utils/axios';
 
-export const getSessions = () =>
-  axios({
-    method: 'get',
-    url: `${process.env.API_URL}/sessions`
-  });
+export const getSessions = () => api.get('sessions');
 
-export const createSession = (email, password) =>
-  axios({
-    url: `${process.env.API_URL}/sessions`,
-    method: 'post',
-    data: { email, password }
-  });
+export const createSession = (email, password) => api.post('sessions', { email, password });
 
-export const deleteSessions = () =>
-  axios({
-    method: 'delete',
-    url: `${process.env.API_URL}/sessions`
-  });
+export const deleteSessions = () => api.delete('sessions');
