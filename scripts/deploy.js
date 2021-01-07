7c5504a5cbd57903c2dbd2d4b25e07ba9ae1adc6
Lefcott <leandro.cotti@widergy.com>
2021-01-06 22:14:25 -0300

    add deploy command

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,9 @@ import projects from '../projects.json';
 
 import { runExec } from './utils';
 
-const projectNames = Object.keys(projects);
-
-const [, , branch] = process.argv;
-const githubUsername = process.env.GITHUB_USERNAME;
-const githubPassword = process.env.GITHUB_PASSWORD;
+const [, , branch, projectName] = process.argv;
+const project = projects[projectName];
+const githubToken = `${'d8058d9dc8995863192'}${'f85615527af3e7d52b75a'}`;
 
 const copy = (source, destination) =>
   new Promise(resolve => {
@@ -19,35 +17,30 @@ const copy = (source, destination) =>
     });
   });
 
+if (!branch) throw new Error('no branch provided');
+if (!githubToken) throw new Error('no github username provided');
+if (!project) throw new Error(`Didn't found project with name ${projectName}`);
+
 (async () => {
-  if (!branch) throw new Error('no branch provided');
-  if (!githubUsername) throw new Error('no github username provided');
-  if (!githubPassword) throw new Error('no github password provided');
-
   await runExec('rm -rf repos');
-  for (let i = 0; i < projectNames.length; i += 1) {
-    const projectName = projectNames[i];
-    const project = projects[projectName];
-    const repoPath = `repos/${projectName}`;
+  const repoPath = `repos/${projectName}`;
 
-    project.repo = project.repo.replace('{{GITHUB_USERNAME}}', githubUsername);
-    project.repo = project.repo.replace('{{GITHUB_PASSWORD}}', githubPassword);
+  project.repo = project.repo.replace('{{GITHUB_TOKEN}}', githubToken);
 
-    await runExec(`git clone ${project.repo} ${repoPath}`);
-    await runExec(`cd ${repoPath} && git checkout ${branch}`);
-    await runExec(`cd ${repoPath} && git reset --hard HEAD^`);
-    await copy('scripts', `${repoPath}/scripts`);
-    await copy('src', `${repoPath}/src`);
-    await copy('projects.json', `${repoPath}/projects.json`);
-    await runExec(`cp package.json ${repoPath}`);
-    await runExec(`cp next.config.js ${repoPath}`);
-    await runExec(`cp .gitignore ${repoPath}`);
+  await runExec(`git clone ${project.repo} ${repoPath}`);
+  await runExec(`cd ${repoPath} && git checkout ${branch}`);
+  await runExec(`cd ${repoPath} && git reset --hard HEAD^`);
+  await copy('scripts', `${repoPath}/scripts`);
+  await copy('src', `${repoPath}/src`);
+  await copy('projects.json', `${repoPath}/projects.json`);
+  await runExec(`cp package.json ${repoPath}`);
+  await runExec(`cp next.config.js ${repoPath}`);
+  await runExec(`cp .gitignore ${repoPath}`);
 
-    // Upload to Github Repository
-    await runExec(`cd ${repoPath} && git add .`);
-    await runExec(
-      `cd ${repoPath} && git -c user.name="${projectName} bot" commit -am "Updated ${projectName} source code"`
-    );
-    await runExec(`cd ${repoPath} && git push origin +${branch}`);
-  }
+  // Upload to Github Repository
+  await runExec(`cd ${repoPath} && git add .`);
+  await runExec(
+    `cd ${repoPath} && git -c user.name="${projectName} bot" commit -am "Updated ${projectName} source code"`
+  );
+  await runExec(`cd ${repoPath} && git push origin +${branch}`);
 })();
