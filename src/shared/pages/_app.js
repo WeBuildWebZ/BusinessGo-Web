ace79d69734392a17cae7f72094959192480ba85
Lefcott <leandro.cotti@widergy.com>
2021-01-12 03:02:47 -0300

    dispatch project!

--- src/shared/pages/_app.js
+++ src/shared/pages/_app.js
@@ -1,9 +1,25 @@
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { combineReducers, createStore } from 'redux';
 
 import commonReducer from '../reducers';
 import AlertStack from '../../components/AlertStack';
+import { showProject } from '../../services/api/project';
+import { setProject } from '../actions/project';
+
+const ReduxFiller = props => {
+  const dispatch = useDispatch();
+  const { constants } = props;
+
+  showProject(constants.PROJECT_CODE).then(({ data: project }) => {
+    dispatch(setProject(project));
+  });
+  return <div />;
+};
+
+ReduxFiller.propTypes = {
+  constants: PropTypes.shape({ PROJECT_CODE: PropTypes.string.isRequired }).isRequired
+};
 
 const getApp = (reducer, constants, AppendComponent) => {
   const store = createStore(combineReducers({ ...commonReducer, ...reducer }));
@@ -12,6 +28,7 @@ const getApp = (reducer, constants, AppendComponent) => {
     <>
       <AppendComponent />
       <Provider store={store}>
+        <ReduxFiller constants={constants} />
         <AlertStack position={constants.ALERT_STACK_POSITION} />
         <Component {...pageProps} />
       </Provider>
