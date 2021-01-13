e3fb306ccb47de9e51499f88a7fa57a56b078fb8
josezanle <jose.rios.lm@gmail.com>
2021-01-13 11:06:13 -0300

    simple galeria sin back

--- src/projects/bob/pages/galeria.js
+++ src/projects/bob/pages/galeria.js
@@ -1,9 +1,58 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { EffectFade, Navigation } from 'swiper';
+
+SwiperCore.use([EffectFade, Navigation]);
+
+
+
 const Galeria = () => {
     return (
-        <div>
-            <h1>Galeria en construccion..</h1>
+        <section className="page-intro">
+    <Swiper navigation effect="fade" className="swiper-wrapper">
+      <SwiperSlide>
+        <div
+          className="page-intro__slide"
+          style={{
+            backgroundImage: "url('/images/slide-1.jpg')",
+            backgroundPosition: 'bottom'
+          }}
+        >
+          <div className="container">
+            <div className="page-intro__slide__content">
+            </div>
+          </div>
         </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div
+          className="page-intro__slide"
+          style={{ backgroundImage: "url('/images/slide-2.jpg')", backgroundPosition: 'bottom' }}
+        >
+          <div className="container">
+            <div className="page-intro__slide__content">
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div
+          className="page-intro__slide"
+          style={{ backgroundImage: "url('/images/slide-3.jpg')", backgroundPosition: 'bottom' }}
+        >
+          <div className="container">
+            <div className="page-intro__slide__content">
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </section>
     )
 }
 
 export default Galeria;
+
+
+
