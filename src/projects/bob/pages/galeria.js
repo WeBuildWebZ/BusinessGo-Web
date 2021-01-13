48eddc1f6b3bdeb4a2bd8363037522ebf9325ebd
Lefcott <leandro.cotti@widergy.com>
2021-01-13 13:13:28 -0300

    arreglando cagadas :)

--- src/projects/bob/pages/galeria.js
+++ src/projects/bob/pages/galeria.js
@@ -1,37 +1,38 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import PropTypes from 'prop-types';
 import SwiperCore, { EffectFade, Navigation } from 'swiper';
 
 SwiperCore.use([EffectFade, Navigation]);
 
+export async function getStaticProps() {
+  const { default: images } = await import(`../utils/data/images`);
 
+  return { props: { images } };
+}
 
-const Galeria = ({ items }) => {
-    return (
-        <section className="page-intro">
-    <Swiper
-     navigation effect="fade"
-     className="swiper-wrapper"
-     >
+const Galeria = ({ images }) => (
+  <section className="page-intro">
+    <Swiper navigation effect="fade" className="swiper-wrapper">
       <SwiperSlide>
-      <div className="cards">
-          {items?.length &&
-            items.map((i) => {
-              return (
-                <div
-                  className="card"
-                  key={i.title}
-                  title={i.title}
-                  picture={i.image}
-                  link={i.slug}
-                />
-              )
-            })}
+        <div className="cards">
+          {images.length &&
+            images.map(image => (
+              // <div className="card" key={i.title} title={i.title} picture={i.image} link={i.slug} />
+              // lo puse como imagen porque los div no tinen las prop picture ni link
+              <img
+                className="card"
+                key={image.title}
+                title={image.title}
+                src={image.image}
+                link={image.slug}
+              />
+            ))}
         </div>
       </SwiperSlide>
     </Swiper>
 
-    <style jsx>{`
-
+    <style jsx>
+      {`
         .cards {
           display: flex;
           flex-wrap: wrap;
@@ -39,30 +40,21 @@ const Galeria = ({ items }) => {
           align-items: center;
         }
         .card {
-          width:150px;
-          height:200px;
+          width: 150px;
+          height: 200px;
           display: flex;
           flex-wrap: wrap;
           justify-content: center;
           align-items: center;
-          flex-flow:column;
+          flex-flow: column;
         }
-      `}</style>
+      `}
+    </style>
   </section>
-    )
-}
+);
+
+Galeria.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
 export default Galeria;
-// lean arregla el path del js..no tuve tiempo y me fui..
-export async function getStaticProps() {
-  const imagesData = await import(`../utils/images`)
-
-  return {
-    props: {
-      items: imagesData.items,
-    },
-  }
-}
-
-
-
-
