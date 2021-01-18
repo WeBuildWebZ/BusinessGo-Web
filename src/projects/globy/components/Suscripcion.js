eea3103fc071f242543e74cab043132e4c502f76
Lefcott <leandro.cotti@widergy.com>
2021-01-18 12:34:40 -0300

    form de suscripcion

--- src/projects/globy/components/Suscripcion.js
+++ src/projects/globy/components/Suscripcion.js
@@ -1,37 +1,72 @@
+import { useState, useEffect } from 'react';
+import { Button, Modal, PopoverTitle } from 'react-bootstrap';
+
+import FieldRenderer from '../../../components/FieldRenderer';
+import Spinner from '../../../components/Spinner';
+import { PROJECT_CODE } from '../constants';
+import { showForm } from '../../../services/api/form';
 
 const Suscripcion = () => {
-    return (
-        <div className="subs">
-            <h4>Suscribe Gratis!</h4>
-            <style jsx>{`
+  const [modalOpen, setModalOpen] = useState(false);
+  const [suscriptionForm, setSuscriptionForm] = useState(null);
 
-            .subs{
-                width: 100vw;
-                height: 0vh;
-                margin: 2em 0;
-                padding:0 1em;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-            h4{
-                color: #86a8e7;
-                padding:1em 2em;
-                border:4px solid #ebebeb;
-                border-radius:1em;
-            }
-            h4:hover{
-                color: white;
-                background:#86a8e7;
-            }
+  useEffect(() => {
+    showForm(PROJECT_CODE, 'email_suscription').then(({ data: givenForm }) => {
+      setSuscriptionForm(givenForm);
+    });
+  }, []);
 
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
 
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
+  return (
+    <>
+      {modalOpen && suscriptionForm && (
+        <Modal show onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <PopoverTitle>Suscríbete a nuestras últimas novedades!</PopoverTitle>
+          </Modal.Header>
+          <Modal.Body>
+            <FieldRenderer fields={suscriptionForm.fields} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button>Suscribirse</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+      <div className="subs">
+        {!suscriptionForm && <Spinner />}
+        {suscriptionForm && <h4 onClick={handleOpenModal}>Suscríbete Gratis!</h4>}
+        <style jsx>{`
+          .subs {
+            width: 100vw;
+            height: 0vh;
+            margin: 2em 0;
+            padding: 0 1em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+          }
+          h4 {
+            color: #86a8e7;
+            padding: 1em 2em;
+            border: 4px solid #ebebeb;
+            border-radius: 1em;
+          }
+          h4:hover {
+            color: white;
+            background: #86a8e7;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+};
 
-
-            `}</style>
-        </div>
-    )
-}
-
-export default Suscripcion
+export default Suscripcion;
--- src/projects/globy/pages/index.js
+++ src/projects/globy/pages/index.js
@@ -12,31 +12,31 @@ import Suscripcion from '../components/Suscripcion';
 export default function Home() {
   return (
     <div className={styles.container}>
-        <Logo />
-        <Frase />
+      <Logo />
+      <Frase />
 
-        {/* seccion color negro con iconos verdes */}
-        <section>
-          <Section />
-        </section>
+      {/* seccion color negro con iconos verdes */}
+      <section>
+        <Section />
+      </section>
 
-        <section className={styles.image}>
-          <BoxCuatroImages />
-        </section>
+      <section className={styles.image}>
+        <BoxCuatroImages />
+      </section>
 
-        <section className={styles.pregunta}>
-          <Pregunta />
-        </section>
+      <section className={styles.pregunta}>
+        <Pregunta />
+      </section>
 
-        <section className={styles.slide}>
-          <Slide />
-        </section>
+      <section className={styles.slide}>
+        <Slide />
+      </section>
 
-        <section className={styles.backgrounfinal}>
-          <h3>
-            <b>Damos lo Mejor de Nosotros cada dia</b>, para brindarte a ti, el mejor servicio en tu ciudad.
-          </h3>
-        </section>
+      <section className={styles.backgrounfinal}>
+        <h3>
+          <b>Damos lo Mejor de Nosotros cada dia</b>, para brindarte a ti, el mejor servicio en tu ciudad.
+        </h3>
+      </section>
 
       <Logo />
       <Suscripcion />
