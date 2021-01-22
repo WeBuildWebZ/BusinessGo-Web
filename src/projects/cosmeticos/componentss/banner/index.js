cf81b6db3625b931d0b817bc5193e7752c806672
Lefcott <leandro.cotti@widergy.com>
2021-01-21 22:17:12 -0300

    listorteishon

--- src/projects/cosmeticos/componentss/banner/index.js
+++ src/projects/cosmeticos/componentss/banner/index.js
@@ -1,98 +1,72 @@
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
-    "mdbreact";
+import { useEffect, useState } from 'react';
+import {
+  MDBCarousel,
+  MDBCarouselCaption,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBView,
+  MDBContainer
+} from 'mdbreact';
 
 import 'bootstrap-css-only/css/bootstrap.min.css';
 
-
+import { getClientDocuments } from '../../../../services/api/clientDocument';
+import Spinner from '../../../../components/Spinner';
 
 const Banner = () => {
-    return (
-        <MDBContainer>
-            <MDBCarousel
-                activeItem={1}
-                length={3}
-                showControls={true}
-                showIndicators={true}
-                className="z-depth-1 full"
-                slide
-            >
-                <MDBCarouselInner>
-                    <MDBCarouselItem itemId="1">
-                        <MDBView>
-                            <img
-                                src="/images/slide-1.jpg"
-                                alt="First slide"
-                                className="d-block w-70"
+  const [sliderItems, setSliderItems] = useState(null);
 
-                            />
-                        </MDBView>
-                        <MDBCarouselCaption>
-                        <div className="text">
-                            <h2 className="h3-responsive">Mejor Precio del mercado</h2>
-                            <h4 className="h3-responsive">Sistema de Chofer Por Puntos</h4>
-                        </div>
-                        </MDBCarouselCaption>
+  useEffect(() => {
+    getClientDocuments('cosmeticos-main_slider').then(({ data: givenSliderItems }) => {
+      setSliderItems(givenSliderItems);
+    });
+  }, []);
 
-                    </MDBCarouselItem>
+  return (
+    <MDBContainer>
+      <MDBCarousel
+        activeItem={1}
+        length={sliderItems?.length || 0}
+        showControls
+        showIndicators
+        className="z-depth-1 full"
+        slide
+      >
+        <MDBCarouselInner>
+          {!sliderItems && <Spinner />}
+          {sliderItems &&
+            sliderItems.map((sliderItem, key) => (
+              <MDBCarouselItem key={key} itemId={`${key + 1}`}>
+                <MDBView>
+                  <img src={sliderItem.photo} alt="Slide" className="d-block w-70" />
+                </MDBView>
+                <MDBCarouselCaption>
+                  <div className="text">
+                    <h2 className="h3-responsive">{sliderItem.title}</h2>
+                    <h4 className="h3-responsive">{sliderItem.subtitle}</h4>
+                  </div>
+                </MDBCarouselCaption>
+              </MDBCarouselItem>
+            ))}
 
-                    <MDBCarouselItem itemId="2" >
-                        <MDBView>
-                            <img
-                                src="/images/slide-2.jpg"
-                                alt="Second slide"
-                                className="d-block w-70"
-                            />
-                        </MDBView>
-
-                        <MDBCarouselCaption>
-                        <div className="text">
-                            <h2 className="h3-responsive">Mejor Precio del mercado</h2>
-                            <h4 className="h3-responsive">Sistema de Chofer Por Puntos</h4>
-                            </div>
-                        </MDBCarouselCaption>
-
-                    </MDBCarouselItem>
-
-                    <MDBCarouselItem itemId="3" >
-                        <MDBView  >
-                            <img
-                                src="/images/slide-3.jpg"
-                                alt="Third slide"
-                                className="d-block w-70"
-                            />
-                        </MDBView>
-
-                        <MDBCarouselCaption>
-                            <div className="text">
-                            <h2 className="h3-responsive">Mejor Precio del mercado</h2>
-                            <h4 className="h3-responsive">Sistema de Chofer Por Puntos</h4>
-                            </div>
-
-
-                        </MDBCarouselCaption>
-
-                    </MDBCarouselItem>
-
-                    <style jsx>
-                    {`
-
-                    img{
-                        width:100%;
-                        height:100%;
-                        object-fit: cover;
-                    }
-                    .text{
-                        width: 100%;
-                        background:white;
-                        color:black;
-                        border-radius:1em;
-                      }
-
-                    `}
-                    </style>
-                </MDBCarouselInner>
-            </MDBCarousel>
-        </MDBContainer>
-    );
-}
-export default Banner
+          <style jsx>
+            {`
+              img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+              }
+              .text {
+                width: 100%;
+                background: white;
+                color: black;
+                border-radius: 1em;
+              }
+            `}
+          </style>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
+  );
+};
+export default Banner;
