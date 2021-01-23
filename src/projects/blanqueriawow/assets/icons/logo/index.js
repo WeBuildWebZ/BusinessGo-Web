699ae6e98d66667475815dffb39e0c1af1088e54
Lefcott <leandro.cotti@widergy.com>
2021-01-22 23:31:01 -0300

    logofix

--- src/projects/blanqueriawow/assets/icons/logo/index.js
+++ src/projects/blanqueriawow/assets/icons/logo/index.js
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 const Logo = () => (
   <>
-  {/* este mundo es una shit */}
-    <Image
-      src="/images/icons/wow2.png"
-      alt="Picture of the author"
-      width={200}
-      height={200}
-    />
+    <Image src="/images/icons/icon.png" alt="Logo" width={150} height={103.125} className="colored" />
+    <style jsx>
+      {`
+        .colored {
+          background: yellow;
+        }
+      `}
+    </style>
   </>
 );
 
