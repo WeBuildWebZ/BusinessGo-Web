95f2497b21507ef15bae4c5e81829bb65e154033
Lefcott <leandro.cotti@widergy.com>
2021-01-24 18:38:00 -0300

    footer

--- src/projects/cosmeticos/componentss/footer/index.js
+++ src/projects/cosmeticos/componentss/footer/index.js
@@ -1,66 +1,61 @@
+import { useSelector } from 'react-redux';
+
+import Spinner from '../../../../components/Spinner';
 
 const Footer = () => {
-    return (
-        <div className="social">
-            <span className="menu">
-                <h3>About Us </h3>
-                <h5>Client </h5>
-                <h5>Galery </h5>
-                <h5>Contact </h5>
-            </span>
-            <span className="menu">
-                <h3>About Us </h3>
-                <h5>Client </h5>
-                <h5>Galery </h5>
-                <h5>Contact </h5>
-            </span>
-            <span className="menu">
-                <h3>About Us </h3>
-                <h5>Client </h5>
-                <h5>Galery </h5>
-                <h5>Contact </h5>
-            </span>
-    <style jsx>{`
+  const project = useSelector(store => store.project);
 
-
-    .social{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 50vh;
-        background-image:linear-gradient(to left,hsla(0, 0%, 0%, 0.897), hsla(0, 0%, 0%, 0.897)), url(/images/featured-2.jpg);
-        background-position:0;
-        background-size: cover;
-        background-repeat: no-repeat;
-        color:white;
-    }
-    .menu{
-        flex:1;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-flow:column;
-    }
-    .h3{
-        flex:1;
-        padding:.5em;
-    }
-    // ========================================
-    @media(max-width:430px){
-        .social{
-            height: 100vh;
-            flex-flow:column;
-
+  return (
+    <div className="social">
+      {!project && <Spinner />}
+      {project &&
+        project.configuration.footer.sections.map((section, i) => (
+          <span className="menu" key={i}>
+            <h3>{section.title}</h3>
+            {section.options.map((option, ii) => (
+              <a href={option.link} target="_blank" key={ii}>
+                <h5>{option.text}</h5>
+              </a>
+            ))}
+          </span>
+        ))}
+      <style jsx>{`
+        .social {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100vw;
+          height: 50vh;
+          background-image: linear-gradient(to left, hsla(0, 0%, 0%, 0.897), hsla(0, 0%, 0%, 0.897)),
+            url(/images/featured-2.jpg);
+          background-position: 0;
+          background-size: cover;
+          background-repeat: no-repeat;
+          color: white;
         }
-    }
-
-    `}</style>
-        </div>
-    )
-}
+        .menu {
+          flex: 1;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-flow: column;
+        }
+        .h3 {
+          flex: 1;
+          padding: 0.5em;
+        }
+        // ========================================
+        @media (max-width: 430px) {
+          .social {
+            height: 100vh;
+            flex-flow: column;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Footer;
-
