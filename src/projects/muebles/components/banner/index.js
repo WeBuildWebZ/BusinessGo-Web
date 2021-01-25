6e0e13bf9913d260de18f99ca65560977cabadf4
Lefcott <leandro.cotti@widergy.com>
2021-01-25 19:22:44 -0300

    fix

--- src/projects/muebles/components/banner/index.js
+++ src/projects/muebles/components/banner/index.js
@@ -9,6 +9,7 @@ export const Banner = () => (
           width: 100vw;
           height: 90vh;
           background-image: url(/images/banner.jpg);
+          filter: blur(1px);
           display: flex;
           justify-content: center;
           align-items: flex-start;
@@ -18,6 +19,7 @@ export const Banner = () => (
         h1,
         h4 {
           color: white;
+          text-shadow: 0 0 15px black;
         }
         h1 {
           font-size: 5em;
