0c823bc3d5219c24c96a3a15fd67cae5c33e4988
Lefcott <leandro.cotti@widergy.com>
2021-01-25 13:52:44 -0300

    fix

--- src/projects/muebles/constants.js
+++ src/projects/muebles/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:4000';
-export const BUCKET_URL = 'https://businessgo-web.s3.amazonaws.com/admin';
-export const URL = 'https://www.muebles.dancotll.com';
+export const PROJECT_CODE = 'cosmeticos';
+export const ALERT_STACK_POSITION = 'bottomRight';
+export const URL = 'https://www.cosmeticos.dancotll.com';
