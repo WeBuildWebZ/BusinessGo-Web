5b4c08a5d48e494f743eb0ed5b48960fd075803a
Lefcott <leandro.cotti@widergy.com>
2021-02-01 01:43:40 -0300

    habilitado cuando esta deployado

--- src/components/Chatbot/utils.js
+++ src/components/Chatbot/utils.js
@@ -1,5 +1,4 @@
 export const shouldRender = project => {
-  if (process.env.NODE_ENV !== 'development') return false;
   if (!project?.chatbot.enabled) return false;
   if (process.browser) {
     if (window.location.pathname === '/admin') return false;
