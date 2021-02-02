e2385a9f2b0dbe1e98f9e399b4b6767e17a47201
Lefcott <leandro.cotti@widergy.com>
2021-02-02 03:26:55 -0300

    fix

--- src/services/chatbot_api/web_message.js
+++ src/services/chatbot_api/web_message.js
@@ -1,4 +1,4 @@
 import api from '../../utils/axios';
 
 export const createWebMessage = (project, text) =>
-  api.post(`chatbot_api/projects/${encodeURIComponent(project.code)}/web_message`, { type: 'text', text });
+  api.post(`chatbot_api/projects/${encodeURIComponent(project.code)}/web_messages`, { type: 'text', text });
