68fcd98daac32f312b1ccc0318d0a0d30e5da5b9
Lefcott <leandro.cotti@widergy.com>
2021-02-06 15:38:09 -0300

    traducciones

--- src/projects/landing/components/Banner/left/index.js
+++ src/projects/landing/components/Banner/left/index.js
@@ -1,107 +1,102 @@
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-const Left = props => (
-  <div className="left">
-    <span className="top" />
-    <span className="down">
-      <h5 className="title">{props.banner.title}</h5>
-      <span>
-        <button className="seeButton" onClick={props.onClickSee}>
-          {props.banner.button}
-        </button>
-        <h6>{props.banner.description}</h6>
+import getLanguage from './lang';
+
+const Left = props => {
+  const language = getLanguage(useSelector(store => store.language));
+
+  return (
+    <div className="left">
+      <span className="top" />
+      <span className="down">
+        <h5 className="title">{language.title}</h5>
+        <span>
+          <button className="seeButton">{language.button}</button>
+          <h6>{language.description}</h6>
+        </span>
       </span>
-    </span>
 
-    <style jsx>
-      {`
-        .left {
-          width: 600px;
-          height: 700px;
-          display: flex;
-          margin-right: 3em;
-          flex-flow: column;
-        }
-        .top,
-        .down {
-          flex: 1;
-          width: 100%;
-          height: 100%;
-        }
-        .top {
-          background-image: linear-gradient(to top, hsla(0, 0%, 100%, 1), hsla(0, 0%, 100%, 0.1)),
-            url(${props.banner.image});
-          background-size: cover;
-          background-position: 0 0;
-          background-repeat: no-repeat;
-          border-radius: 1em;
-        }
-        // ================================
-        .down {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-flow: column;
-        }
-        .title {
-          font-size: 2.5em;
-          flex: 1;
-          margin-top: 1.3em;
-          overflow-y: hidden;
-        }
-        .down span {
-          flex: 1;
-          width: 100%;
-          height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .seeButton {
-          padding: 0.3em 3.5em;
-          margin-right: 1em;
-          outline: none;
-          border: none;
-          background: #1c1c1c;
-          color: white;
-          border-radius: 0.3em;
-          transition: all 0.2s ease-in-out;
-        }
-        .seeButton:hover {
-          transform: scale(1.1);
-          border-radius: 0;
-        }
-        h6 {
-          padding-top: 7px;
-        }
-        // =====================================================================
-        @media (max-width: 1286px) {
+      <style jsx>
+        {`
           .left {
-            margin-right: 0;
+            width: 600px;
+            height: 700px;
+            display: flex;
+            margin-right: 3em;
+            flex-flow: column;
           }
-        }
-        @media (max-width: 605px) {
-          .left {
-            width: 100vw;
+          .top,
+          .down {
+            flex: 1;
+            width: 100%;
+            height: 100%;
           }
-        }
-        @media (max-width: 388px) {
+          .top {
+            background-image: linear-gradient(to top, hsla(0, 0%, 100%, 1), hsla(0, 0%, 100%, 0.1)),
+              url(/images/left-top.jpg);
+            background-size: cover;
+            background-position: 0 0;
+            background-repeat: no-repeat;
+            border-radius: 1em;
+          }
+          // ================================
+          .down {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-flow: column;
+          }
+          .title {
+            font-size: 2.5em;
+            flex: 1;
+            margin-top: 1.3em;
+            overflow-y: hidden;
+          }
           .down span {
-            flex-flow: column-reverse;
+            flex: 1;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
           }
-        }
-      `}
-    </style>
-  </div>
-);
-
-Left.propTypes = {
-  banner: PropTypes.object.isRequired,
-  onClickSee: PropTypes.func
-};
-
-Left.defaultProps = {
-  onClickSee: () => {}
+          .seeButton {
+            padding: 0.3em 3.5em;
+            margin-right: 1em;
+            outline: none;
+            border: none;
+            background: #1c1c1c;
+            color: white;
+            border-radius: 0.3em;
+            transition: all 0.2s ease-in-out;
+          }
+          .seeButton:hover {
+            transform: scale(1.1);
+            border-radius: 0;
+          }
+          h6 {
+            padding-top: 7px;
+          }
+          // =====================================================================
+          @media (max-width: 1286px) {
+            .left {
+              margin-right: 0;
+            }
+          }
+          @media (max-width: 605px) {
+            .left {
+              width: 100vw;
+            }
+          }
+          @media (max-width: 388px) {
+            .down span {
+              flex-flow: column-reverse;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
 };
 
 export default Left;
