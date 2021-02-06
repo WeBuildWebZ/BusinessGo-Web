25e9ed56816a83342d11511390f66645660f7632
Lefcott <leandro.cotti@widergy.com>
2021-02-06 17:12:17 -0300

    traducciones

--- src/projects/landing/components/items-explicacion/index.js
+++ src/projects/landing/components/items-explicacion/index.js
@@ -1,195 +1,158 @@
+import { useSelector } from 'react-redux';
+
+import { getLanguage } from './lang';
+
 const iconColors = ['#F9EE08', '#FF8CAC', '#00E6FF'];
 
-const items = [
-  {
-    id: 1,
-    image: '/bullhorn-solid.svg',
-    title: 'SEO',
-    description:
-      'Es la dispocion que debe tener en su estructura, para poder ser encontrada en los buscadores e Google, por ej, a mayor calidad de SEO, mas eficacia y posicionamiento'
-  },
-  {
-    id: 2,
-    image: '/feather-alt-solid.svg',
-    title: ' CopyWriting',
-    description:
-      'Es redaccion de textos que estimulan en los lectores un movimiento de compra, es usado por las empresas para generar oportunidades de negocios'
-  },
-  {
-    id: 3,
-    image: '/gem-regular.svg',
-    title: 'Exclusividad de Diseños',
-    description:
-      'Es la dispocion que debe tener en su estructura, para poder ser encontrada en los buscadores e Google, por ej, a mayor calidad de SEO, mas eficacia y posicionamiento'
-  },
+const ItemsExplicacion = () => {
+  const language = getLanguage(useSelector(store => store.language));
 
-  {
-    id: 4,
-    image: '/handshake-solid.svg',
-    title: 'Metodos de Pago',
-    description:
-      'Es la dispocion que debe tener en su estructura, para poder ser encontrada en los buscadores e Google, por ej, a mayor calidad de SEO, mas eficacia y posicionamiento'
-  },
-  {
-    id: 5,
-    image: '/key-solid.svg',
-    title: 'Auto Administrables',
-    description:
-      'Es la dispocion que debe tener en su estructura, para poder ser encontrada en los buscadores e Google, por ej, a mayor calidad de SEO, mas eficacia y posicionamiento'
-  },
-  {
-    id: 6,
-    image: '/images-solid.svg',
-    title: 'Optimizacion de Imagenes',
-    description:
-      'Es la dispocion que debe tener en su estructura, para poder ser encontrada en los buscadores e Google, por ej, a mayor calidad de SEO, mas eficacia y posicionamiento'
-  }
-];
-// con este metodo retornamos las 3 primeras posiciones del array,en total son 6
-const primeras3 = items.slice(0, 3);
+  // con este metodo retornamos las 3 primeras posiciones del array,en total son 6
+  const primeras3 = language.items.slice(0, 3);
 
-// con este metodo retornamos las 3 ultimas posiciones del array,en total son 6
-const ultimas3 = items.slice(3, 6);
+  // con este metodo retornamos las 3 ultimas posiciones del array,en total son 6
+  const ultimas3 = language.items.slice(3, 6);
 
-const ItemsExplicacion = () => (
-  <>
-    <div className="top">
-      <div className="left">
-        {primeras3.map(({ id, image, title, description }) => (
-          <li className="card" key={id} data-aos="fade-right">
-            <span className="icon">
-              <img src={image} alt="" />
-            </span>
+  return (
+    <>
+      <div className="top">
+        <div className="left">
+          {primeras3.map(({ id, image, title, description }) => (
+            <li className="card" key={id} data-aos="fade-right">
+              <span className="icon">
+                <img src={image} alt="" />
+              </span>
 
-            <span className="text">
-              <h3>{title}</h3>
-              <p>{description}</p>
-            </span>
-          </li>
-        ))}
+              <span className="text">
+                <h3>{title}</h3>
+                <p>{description}</p>
+              </span>
+            </li>
+          ))}
+        </div>
+        <div className="right" data-aos="fade-left">
+          <img src="/happy-one.jpg" alt="" className="image" />
+        </div>
       </div>
-      <div className="right" data-aos="fade-left">
-        <img src="/happy-one.jpg" alt="" className="image" />
+
+      <div className="bottom">
+        <div className="right" data-aos="fade-right">
+          <img src="/happy-two.jpg" alt="" className="image" />
+        </div>
+        <div className="left">
+          {ultimas3.map(({ id, image, title, description }) => (
+            <li className="card" key={id} data-aos="fade-left">
+              <span className="icon">
+                <img src={image} alt="" />
+              </span>
+
+              <span className="text">
+                <h3>{title}</h3>
+                <p>{description}</p>
+              </span>
+            </li>
+          ))}
+        </div>
       </div>
-    </div>
-
-    <div className="bottom">
-      <div className="right" data-aos="fade-right">
-        <img src="/happy-two.jpg" alt="" className="image" />
-      </div>
-      <div className="left">
-        {ultimas3.map(({ id, image, title, description }) => (
-          <li className="card" key={id} data-aos="fade-left">
-            <span className="icon">
-              <img src={image} alt="" />
-            </span>
-
-            <span className="text">
-              <h3>{title}</h3>
-              <p>{description}</p>
-            </span>
-          </li>
-        ))}
-      </div>
-    </div>
-    <style jsx>
-      {`
-        .top,
-        .bottom {
-          width: 100vw;
-          height: 100vh;
-          display: flex;
-          background: #ebebeb;
-        }
-        .left {
-          width: 100%;
-          height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-flow: column;
-          flex: 1;
-        }
-
-        .right {
-          width: 100%;
-          height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex: 1;
-        }
-        .image {
-          width: 500px;
-          height: 500px;
-          border-radius: 1em;
-          position: relative;
-        }
-        // ======================================================================
-        .card {
-          width: 70%;
-          display: flex;
-          flex-flow: row;
-          border-radius: 1em;
-          margin: 10px 0;
-          padding: 1em;
-          box-shadow: 0.3em 0.3em 0.5em grey;
-        }
-
-        .icon {
-          width: 100%;
-          height: 100%;
-          display: flex;
-          align-items: center;
-          flex: 2;
-          padding-right: 1em;
-        }
-        .text {
-          width: 500px;
-          flex: 8;
-        }
-        img {
-          width: 50px;
-          height: 50px;
-          object-fit: cover;
-        }
-        // ======================
-        @media (max-width: 1022px) {
+      <style jsx>
+        {`
           .top,
           .bottom {
-            height: 100%;
-            flex-flow: column-reverse;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            background: #ebebeb;
           }
-          .card {
+          .left {
+            width: 100%;
             height: 100%;
-            padding: 1em;
-            box-shadow: 0.3em 0.3em 0.5em silver;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-flow: column;
+            flex: 1;
           }
-          .left,
+
           .right {
             width: 100%;
             height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex: 1;
           }
-        }
-        @media (max-width: 754px) {
+          .image {
+            width: 500px;
+            height: 500px;
+            border-radius: 1em;
+            position: relative;
+          }
+          // ======================================================================
           .card {
+            width: 70%;
+            display: flex;
+            flex-flow: row;
+            border-radius: 1em;
+            margin: 10px 0;
+            padding: 1em;
+            box-shadow: 0.3em 0.3em 0.5em grey;
+          }
+
+          .icon {
             width: 100%;
             height: 100%;
-          }
-        }
-        @media (max-width: 534px) {
-          .card {
-            width: 100vw;
-            flex-flow: column;
-            padding: 0.5em;
+            display: flex;
+            align-items: center;
+            flex: 2;
+            padding-right: 1em;
           }
           .text {
-            width: 100%;
+            width: 500px;
+            flex: 8;
           }
-        }
-      `}
-    </style>
-  </>
-);
+          img {
+            width: 50px;
+            height: 50px;
+            object-fit: cover;
+          }
+          // ======================
+          @media (max-width: 1022px) {
+            .top,
+            .bottom {
+              height: 100%;
+              flex-flow: column-reverse;
+            }
+            .card {
+              height: 100%;
+              padding: 1em;
+              box-shadow: 0.3em 0.3em 0.5em silver;
+            }
+            .left,
+            .right {
+              width: 100%;
+              height: 100%;
+            }
+          }
+          @media (max-width: 754px) {
+            .card {
+              width: 100%;
+              height: 100%;
+            }
+          }
+          @media (max-width: 534px) {
+            .card {
+              width: 100vw;
+              flex-flow: column;
+              padding: 0.5em;
+            }
+            .text {
+              width: 100%;
+            }
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
 export default ItemsExplicacion;
---
+++ src/projects/landing/components/items-explicacion/lang.js
@@ -0,0 +1,79 @@
+export const getLanguage = langCode =>
+  ({
+    es: {
+      items: [
+        {
+          image: '/bullhorn-solid.svg',
+          title: 'SEO',
+          description:
+            'Es la dispoción que debe tener la web en su estructura, para poder ser encontrada en los buscadores como Google. A mayor calidad de SEO, más eficacia y posicionamiento.'
+        },
+        {
+          image: '/feather-alt-solid.svg',
+          title: ' CopyWriting',
+          description:
+            'Es redacción de textos que estimulan en los lectores un movimiento de compra. Es usado por las empresas para generar oportunidades de negocios.'
+        },
+        {
+          image: '/gem-regular.svg',
+          title: 'Exclusividad de Diseños',
+          description: 'Ofrecemos diseños pensandos exclusivamente para tu negocio.'
+        },
+        {
+          image: '/handshake-solid.svg',
+          title: 'Métodos de Pago',
+          description:
+            'Te damos la posibilidad de que tu web tenga métodos de pago incorporados como Paypal y Mercado Pago.'
+        },
+        {
+          image: '/key-solid.svg',
+          title: 'Auto Administrables',
+          description:
+            'Tú podrás administrar el contenido de tu web, desde imágenes, textos hasta elementos de tu negocio en particular, como productos, eventos, etc.'
+        },
+        {
+          image: '/images-solid.svg',
+          title: 'Optimizacion de Imágenes',
+          description: 'Optimizamos las imágenes!'
+        }
+      ]
+    },
+    en: {
+      items: [
+        {
+          image: '/bullhorn-solid.svg',
+          title: 'SEO',
+          description:
+            'It is the layout that the web must have in its structure, in order to be found in search engines such as Google. The higher the quality of SEO, the more efficiency and positioning.'
+        },
+        {
+          image: '/feather-alt-solid.svg',
+          title: ' CopyWriting',
+          description:
+            'It is the writing of texts that stimulate a buying movement in the readers. It is used by companies to generate business opportunities.'
+        },
+        {
+          image: '/gem-regular.svg',
+          title: 'Design Exclusiveness',
+          description: 'We offer designs exclusively for your business.'
+        },
+        {
+          image: '/handshake-solid.svg',
+          title: 'Payment Methods',
+          description:
+            'We give you the possibility that your website has payment methods incorporated such as Paypal and Mercado Pago.'
+        },
+        {
+          image: '/key-solid.svg',
+          title: 'Manageable Websites',
+          description:
+            'You can manage the content of your website, from images, texts to elements of your particular business, such as products, events, etc.'
+        },
+        {
+          image: '/images-solid.svg',
+          title: 'Image Optimization',
+          description: 'We optimize images!'
+        }
+      ]
+    }
+  }[langCode]);
