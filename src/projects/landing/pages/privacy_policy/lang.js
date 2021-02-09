6c6e0edd10a6fccba69414a64aad5d9cc6be921d
Lefcott <leandro.cotti@widergy.com>
2021-02-08 23:17:37 -0300

    listoo

--- src/projects/landing/pages/privacy_policy/lang.js
+++ src/projects/landing/pages/privacy_policy/lang.js
@@ -2,26 +2,21 @@ export const getLanguage = langCode =>
   ({
     es: {
       privacyPolicy: 'Política de Privacidad',
-      privacyPolicyContent: `Cuando precisemos obtener información por su parte, siempre le solicitaremos que nos la proporcione voluntariamente de forma expresa. Los datos recabados a través de los formularios de recogida de datos del sitio Web u otras vías serán incorporados a un fichero de datos de carácter personal debidamente inscrito en el Registro General de Protección de Datos de la Agencia Española de Protección de Datos, del cual es responsable EL PROPIETARIO DE LA Web. Esta entidad tratará los datos de forma confidencial y exclusivamente con la finalidad de ofrecer los servicios solicitados, con todas las garantías legales y de seguridad que impone la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal, el Real Decreto 1720/2007, de 21 de diciembre y la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico.
-      EL PROPIETARIO DE LA Web se compromete a no ceder, vender, ni compartir los datos con terceros sin su aprobación expresa.
-      Asimismo, ASIER LOPEZ CABAÑAS cancelará o rectificará los datos cuando resulten inexactos, incompletos o hayan dejado de ser necesarios o pertinentes para su finalidad, de conformidad con lo previsto en la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal.
-      El usuario podrá revocar el consentimiento prestado y ejercer los derechos de acceso, rectificación, cancelación y oposición dirigiéndose a tal efecto al domicilio social de ASIER LOPEZ CABAÑAS, sito en CARRER PERE DE LLUNA Nº 22 3º A – 43204 REUS - TARRAGONA, identificándose debidamente e indicando de forma visible el concreto derecho que se ejerce.
-      EL PROPIETARIO DE LA Web adopta los niveles de seguridad correspondientes requeridos por la citada Ley Orgánica 15/1999 y demás normativa aplicable. No obstante, no asume ninguna responsabilidad por los daños y perjuicios derivados de alteraciones que terceros pueden causar en los sistemas informáticos, documentos electrónicos o ficheros del usuario.
-      EL PROPIETARIO DE LA Web podrá utilizar cookies durante la prestación de servicios del sitio Web. Las cookies son ficheros físicos de información personal alojados en el propio terminal del usuario. El usuario tiene la posibilidad de configurar su programa navegador de manera que se impida la creación de archivos cookie o se advierta de la misma.
-      Si opta a abandonar nuestro sitio Web a través de enlaces a sitios Web no pertenecientes a nuestra entidad, EL PROPIETARIO DE LA Web no se hará responsable de las políticas de privacidad de dichos sitios Web ni de las cookies que éstos puedan almacenar en el ordenador del usuario.
-      Nuestra política con respecto al correo electrónico se centra en remitir únicamente comunicaciones que usted haya solicitado recibir.
-      Si prefiere no recibir estos mensajes por correo electrónico le ofreceremos a través de los mismos la posibilidad de ejercer su derecho de cancelación y renuncia a la recepción de estos mensajes, en conformidad con lo dispuesto en el Título III, artículo 22 de la Ley 34/2002 de Servicios para la Sociedad de la Información y de Comercio Electrónico.
-
-      5. PROCEDIMIENTO EN CASO DE REALIZACIÓN DE ACTIVIDADES DE CARÁCTER ILÍCITO
-      En el caso de que cualquier usuario o un tercero considere que existen hechos o circunstancias que revelen el carácter ilícito de la utilización de cualquier contenido y/o de la realización de cualquier actividad en las páginas Web incluidas o accesibles a través del sitio Web, deberá enviar una notificación a EL PROPIETARIO DE LA Web identificándose debidamente, especificando las supuestas infracciones y declarando expresamente y bajo su responsabilidad que la información proporcionada en la notificación es exacta.
-      Para toda cuestión litigiosa que incumba al sitio Web de EL PROPIETARIO DE LA Web, será de aplicación la legislación española, siendo competentes los Juzgados y Tribunales de TARRAGONA (España).
-
-      6. PUBLICACIONES
-      La información administrativa facilitada a través del sitio Web no sustituye la publicidad legal de las leyes, normativas, planes, disposiciones generales y actos que tengan que ser publicados formalmente a los diarios oficiales de las administraciones públicas, que constituyen el único instrumento que da fe de su autenticidad y contenido. La información disponible en este sitio Web debe entenderse como una guía sin propósito de validez legal.
+      privacyPolicyContent: `
+        Cuando necesitemos información de su parte, le pediremos que nos la proporcione voluntariamente. Los datos recibidos a través de los formularios sólo los podrá consultar usted a través de la sección de administración del proyecto, no los compartiremos con nadie más que usted.
+        Usted podrá utilizar cookies durante la prestación de servicios del sitio Web. Las cookies son ficheros de información personal alojados en el propio terminal del usuario. El usuario tiene la posibilidad de configurar su navegador de manera que se impida la creación de archivos cookie o se advierta de la misma.
+        Nuestra política con respecto al correo electrónico se centra en remitir únicamente comunicaciones que usted haya solicitado recibir.
+        Si prefiere no recibir estos mensajes por correo electrónico le ofreceremos la cancelación de los mismos.
       `
     },
     en: {
-      privacyPolicy: 'Privacy Policy'
+      privacyPolicy: 'Privacy Policy',
+      privacyPolicyContent: `
+        When we need information from you, we will ask you to voluntarily provide it to us. The data received through the forms can only be consulted by you through the project administration section, we will not share it with anyone other than you.
+        You may use cookies during the provision of services on the website. Cookies are files of personal information hosted in the user's own terminal. The user has the possibility of configuring their browser in such a way as to prevent the creation of cookie files or warn of it.
+        If you prefer not to receive these messages by email, we will offer to cancel them.
+        .
+      `
     }
   }[langCode]);
 
