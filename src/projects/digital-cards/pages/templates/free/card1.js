bb145220abfeda0cfd2db805b6de5c39a7a42d95
josezanle <jose.rios.lm@gmail.com>
2021-02-25 12:23:40 -0300

    MN-10 component Card1 free demo

---
+++ src/projects/digital-cards/pages/templates/free/card1.js
@@ -0,0 +1,104 @@
+const Card1 = () => (
+  <div className="card1">
+    <div className="top">
+      <h2>Nike</h2>
+      <p>Explore</p>
+    </div>
+    <div className="bottom">
+      <img src="/icon/facebook.svg" alt="" />
+      <img src="/icon/instagram.svg" alt="" />
+      <img src="/icon/messenger.svg" alt="" />
+      <img src="/icon/linkedin.svg" alt="" />
+    </div>
+    <div className="select">
+      <img src="/icon/hand.svg" alt="" />
+      <h5>Presione un icono para elegir.</h5>
+    </div>
+    <style jsx>
+      {`
+        .card1 {
+          width: 18.5em;
+          height: 32em;
+          border: 0.2em solid #ebebeb;
+          border-radius: 1em;
+          padding: 0.5em;
+          margin: 0.5em;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-flow: column;
+          cursor: pointer;
+        }
+        .card1:hover {
+          box-shadow: 0.3em 0.3em 0.5em silver;
+        }
+        .top {
+          flex: 4;
+          width: 100%;
+          height: 100%;
+          padding: 0.5em;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-flow: column;
+          position: relative;
+        }
+        h2 {
+          font-size: 7em;
+          font-weight: bold;
+          transform: rotate(-90deg);
+        }
+        p {
+          position: absolute;
+          bottom: 0;
+          background: #ebebeb;
+          font-weight: bold;
+          width: 50%;
+          text-align: center;
+          padding: 0.3em 0;
+          letter-spacing: 0.5em;
+        }
+
+        //   =========================================================================
+
+        .bottom {
+          flex: 1;
+          width: 100%;
+          height: 100%;
+          padding: 0.5em;
+          display: flex;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+        .bottom img {
+          width: 30px;
+          height: 30px;
+        }
+        // ===========================================================================
+
+        .select {
+          flex: 1;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-image: linear-gradient(to right, #2e2e2e 0%, black 100%);
+          color: white;
+        }
+        h5 {
+          font-size: 0.9em;
+        }
+        .select img {
+          width: 25px;
+          height: 25px;
+          margin-right: 10px;
+          background: white;
+          border-radius: 0.5em;
+        }
+      `}
+    </style>
+  </div>
+);
+
+export default Card1;
