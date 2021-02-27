473e09ea30f05338cae5b48d9d1c949f88511672
Lefcott <leandro.cotti@widergy.com>
2021-02-27 17:09:46 -0300

    arreglado!

--- src/projects/digital-cards/components/banner/right/index.js
+++ src/projects/digital-cards/components/banner/right/index.js
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import useScrollTop from '../../../../../shared/hooks/useScrollTop';
 
 const Right = () => {
   const scrollTop = useScrollTop(process.browser ? document.body : null);
+  const [imageTop, setImageTop] = useState(0);
+  const offsetTop = scrollTop - imageTop < 0 ? 0 : scrollTop - imageTop;
+  const imageRef = useRef();
+
+  useEffect(() => {
+    setImageTop(imageRef.current.offsetTop);
+  }, [imageRef.current?.offsetTop]);
 
   return (
     <div className="right">
-      <div className="image" />
+      <div className="image" ref={imageRef} />
       <style jsx>
         {`
           .right {
@@ -17,9 +24,9 @@ const Right = () => {
             align-items: center;
           }
           .image {
-            width: calc(450px - ${scrollTop * 0.8}px);
-            filter: blur(${scrollTop * 0.02}px);
-            transform: rotate(${scrollTop * 0.1}deg);
+            width: calc(450px - ${offsetTop * 0.8}px);
+            filter: blur(${offsetTop * 0.02}px);
+            transform: rotate(${offsetTop * 0.1}deg);
             min-height: 600px;
             background-image: url(/images/banner.jpg);
             background-size: contain;
