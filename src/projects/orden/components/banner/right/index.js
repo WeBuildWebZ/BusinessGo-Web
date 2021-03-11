b8187c2a170d78e36dd9af478147c9c98c7599a8
Lefcott <leandro.cotti@widergy.com>
2021-03-11 20:41:41 -0300

    foto

--- src/projects/orden/components/banner/right/index.js
+++ src/projects/orden/components/banner/right/index.js
@@ -1,37 +1,43 @@
-const Right = () => (
-  <div className="image">
-    <img src="/images/she.jpg" alt="ñalal" />
-    <style jsx>
-      {`
-        .image {
-          min-width: 600px;
-          min-height: 90vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 300;
-        }
-        img {
-          width: 550px;
-          height: 550px;
-          object-fit: cover;
-          border-radius: 0 8em;
-          -moz-transform: scaleX(-1);
-          -o-transform: scaleX(-1);
-          -webkit-transform: scaleX(-1);
-          transform: scaleX(-1);
-        }
+import { useSelector } from 'react-redux';
 
-        // =========================================
-        @media (max-width: 550px) {
-          img {
-            width: 100vw;
-            height: 550px;
+const Right = () => {
+  const project = useSelector(store => store.project);
+
+  return (
+    <div className="image">
+      {project && <img src={project.configuration.basic_info.photo} alt="she" />}
+      <style jsx>
+        {`
+          .image {
+            min-width: 600px;
+            min-height: 90vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 300;
           }
-        }
-      `}
-    </style>
-  </div>
-);
+          img {
+            width: 550px;
+            height: 550px;
+            object-fit: cover;
+            border-radius: 0 8em;
+            -moz-transform: scaleX(-1);
+            -o-transform: scaleX(-1);
+            -webkit-transform: scaleX(-1);
+            transform: scaleX(-1);
+          }
+
+          // =========================================
+          @media (max-width: 550px) {
+            img {
+              width: 100vw;
+              height: 550px;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default Right;
