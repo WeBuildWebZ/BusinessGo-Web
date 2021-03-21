42174eac4170d12b8b4bf4dea6bf0eed82840eda
Lefcott <leandro.cotti@widergy.com>
2021-03-20 21:34:18 -0300

    clientes

--- src/projects/digital-cards/pages/clientes/index.js
+++ src/projects/digital-cards/pages/clientes/index.js
@@ -3,12 +3,20 @@ import Layout from '../layout';
 const Clientes = () => (
   <Layout>
     <div className="clientes">
-      <h3>clientes</h3>
+      <h3 className="message">Aquí colocaremos imágenes de las primeras MiniWebs de los clientes.</h3>
       <style jsx>
         {`
           .clientes {
             width: 100vw;
             height: 100vh;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+          }
+          .message {
+            color: #c800cd;
           }
         `}
       </style>
