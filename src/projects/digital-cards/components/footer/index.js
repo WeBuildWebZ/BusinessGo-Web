c2eae2fb82ee86e1e318ab29cc0dfe017436c251
Lefcott <leandro.cotti@widergy.com>
2021-03-20 21:57:01 -0300

    footer configurable

--- src/projects/digital-cards/components/footer/index.js
+++ src/projects/digital-cards/components/footer/index.js
@@ -1,137 +1,125 @@
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
-const Footer = () => (
-  <div className="footer">
-    <div className="bottom">
-      <span className="column">
-        <h3>
-          <Link href="/">
-            <a className="pointer">Preguntas Frecuentes</a>
-          </Link>
-        </h3>
-      </span>
+const Footer = () => {
+  const project = useSelector(store => store.project);
 
-      <span className="column">
-        <h3>
-          <Link href="/">
-            <a className="pointer">Términos y Condiciones</a>
-          </Link>
-        </h3>
-      </span>
+  return (
+    <div className="footer">
+      <div className="bottom">
+        {project?.configuration.footer.sections.map((section, i) => (
+          <span key={i} className="column">
+            <h3>
+              <Link href={section.link}>
+                <a className="pointer">{section.title}</a>
+              </Link>
+            </h3>
+            <p>{section.description}</p>
+          </span>
+        ))}
 
-      <span className="column">
-        <h3>
-          <Link href="/">
-            <a className="pointer">Aviso de Privacidad</a>
-          </Link>
-        </h3>
-      </span>
-
-      <span className="column">
-        <h3>Información de Contacto</h3>
-        <span className="row">
-          <img src="/icon/house.png" alt="house icon" />
-          <p> Mexico- Mexico</p>
-          <br />
+        <span className="column">
+          <h3>{project?.configuration.footer.contact_info_title}</h3>
+          <span className="row">
+            <img src="/icon/house.png" alt="house icon" />
+            <p>{project?.configuration.footer.location}</p>
+            <br />
+          </span>
+          <span className="row">
+            <img src="/icon/phone.png" alt="phone icon" />
+            <p>
+              {project?.configuration.footer.phone && (
+                <a href={`tel:${project?.configuration.footer.phone}`} target="blank">
+                  {project?.configuration.footer.phone}
+                </a>
+              )}
+            </p>
+          </span>
+          <span className="row">
+            <img src="/icon/contact.png" alt="contact icon" />
+            <p>
+              {project?.configuration.footer.email && (
+                <a href={`mailto:${project?.configuration.footer.email}`} target="blank">
+                  {project?.configuration.footer.email}
+                </a>
+              )}
+            </p>
+          </span>
         </span>
-        <span className="row">
-          <img src="/icon/phone.png" alt="phone icon" />
-          <p>
-            <a href="tel:1162329888" target="blank">
-              1234567890
-            </a>
-          </p>
-        </span>
-        <span className="row">
-          <img src="/icon/contact.png" alt="contact icon" />
-          <p>
-            <a href="mailto:lefcott@hotmail.com" target="blank">
-              usted@gmail.com
-            </a>
-          </p>
-        </span>
-        <span className="row">
-          <img src="/icon/privacy_policy.png" alt="contact icon" />
-          <p>
-            <Link href="/privacy_policy">
-              <a>Privacy Policy</a>
-            </Link>
-          </p>
-        </span>
-      </span>
-    </div>
+      </div>
 
-    <style jsx>
-      {`
-        .footer {
-          width: 100vw;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background: #0f0f0f;
-        }
-
-        .bottom {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex: 1;
-          width: 100%;
-          height: 100%;
-        }
-
-        .column {
-          width: 400px;
-          height: 100%;
-          padding: 1em;
-          border-radius: 1em;
-        }
-        .row {
-          width: 100%;
-          display: flex;
-          padding: 0.5em 0 0 0.5em;
-        }
-        h3 {
-          margin-top: 1em;
-          color: white;
-        }
-        img {
-          width: 25px;
-          height: 25px;
-        }
-        p {
-          color: grey;
-          margin: 0;
-          padding: 0 0 0 0.5em;
-        }
-        .pointer {
-          cursor: pointer;
-          color: white;
-        }
-
-        a {
-          color: grey;
-        }
-
-        @media (max-width: 840px) {
-          .bottom {
-            flex-flow: column;
-          }
-          .column {
-            width: 100%;
-            padding: 1em;
-            border-radius: 0;
+      <style jsx>
+        {`
+          .footer {
+            width: 100vw;
             display: flex;
             justify-content: center;
-            flex-flow: column;
+            align-items: center;
+            background: #0f0f0f;
+          }
+
+          .bottom {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex: 1;
+            width: 100%;
+            height: 100%;
+          }
+
+          .column {
+            width: 400px;
+            height: 100%;
+            padding: 1em;
+            border-radius: 1em;
           }
           .row {
-            padding: 0 0 1em 1em;
+            width: 100%;
+            display: flex;
+            padding: 0.5em 0 0 0.5em;
           }
-        }
-      `}
-    </style>
-  </div>
-);
+          h3 {
+            margin-top: 1em;
+            color: white;
+          }
+          img {
+            width: 25px;
+            height: 25px;
+          }
+          p {
+            color: grey;
+            margin: 0;
+            padding: 0 0 0 0.5em;
+          }
+          .pointer {
+            cursor: pointer;
+            color: white;
+          }
+
+          a {
+            color: grey;
+          }
+
+          @media (max-width: 840px) {
+            .bottom {
+              flex-flow: column;
+            }
+            .column {
+              width: 100%;
+              padding: 1em;
+              border-radius: 0;
+              display: flex;
+              justify-content: center;
+              flex-flow: column;
+            }
+            .row {
+              padding: 0 0 1em 1em;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default Footer;
