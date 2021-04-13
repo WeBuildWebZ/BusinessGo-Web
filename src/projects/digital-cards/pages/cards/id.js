c16e703933a916f6e345f794930de5701111f6d2
lefcott <lefcott@hotmail.com>
2021-04-12 23:06:06 -0300

    almost finished '

--- src/projects/digital-cards/pages/cards/id.js
+++ src/projects/digital-cards/pages/cards/id.js
@@ -1,32 +1,28 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// import Spinner from '../../../../components/Spinner';
-// import Card from '../../components/Card2';
-// import { showClientDocument } from '../../../../services/api/clientDocument';
+import Spinner from '../../../../components/Spinner';
+import Card from '../../components/Card2';
+import { showClientDocument } from '../../../../services/api/clientDocument';
 
-// const Cards = () => {
-//   const [card, setCard] = useState(null);
-//   const cardId = typeof window === 'object' ? window.location.pathname.substr('/cards/'.length) : null;
+const Cards = () => {
+  const [card, setCard] = useState(null);
+  const cardId = typeof window === 'object' ? window.location.pathname.substr('/cards/'.length) : null;
 
-//   console.log('card', card);
+  console.log('card', card);
 
-//   useEffect(() => {
-//     if (!cardId) return;
-//     showClientDocument(cardId).then(({ data: givenCard }) => {
-//       setCard(givenCard);
-//     });
-//   }, [cardId]);
+  useEffect(() => {
+    if (!cardId) return;
+    showClientDocument(cardId).then(({ data: givenCard }) => {
+      setCard(givenCard);
+    });
+  }, [cardId]);
 
-//   return (
-//     <>
-//       {!card && <Spinner />}
-//       {card && <Card card={card} />}
-//     </>
-//   );
-// };
+  return (
+    <>
+      {!card && <Spinner />}
+      {card && <Card card={card} />}
+    </>
+  );
+};
 
-// export default Cards;
-
-const Card = () => <div>card</div>;
-
-export default Card;
+export default Cards;
