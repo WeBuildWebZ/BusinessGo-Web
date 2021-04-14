4d4b912e041aaad02fddef6a7474eeb3f0323b39
lefcott <lefcott@hotmail.com>
2021-04-14 15:34:23 -0300

    switch to carpediem

--- src/projects/carpediem/constants.js
+++ src/projects/carpediem/constants.js
@@ -1,5 +1,5 @@
-export const PROJECT_CODE = 'store';
+export const PROJECT_CODE = 'carpediem';
 export const ALERT_STACK_POSITION = 'bottomRight';
-export const URL = 'https://www.store.webuildwebz.com';
+export const URL = 'https://www.carpediem.webuildwebz.com';
 export const HAS_LOGIN = false;
 export const DEFAULT_LANGUAGE = 'es';
