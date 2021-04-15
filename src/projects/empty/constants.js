4bcd4ab5138605fd8f1d2c98c9b0feaead77a985
lefcott <lefcott@hotmail.com>
2021-04-15 02:00:22 -0300

    change default values

--- src/projects/empty/constants.js
+++ src/projects/empty/constants.js
@@ -2,5 +2,5 @@ export const PROJECT_CODE = 'empty';
 export const ALERT_STACK_POSITION = 'bottomRight';
 export const URL = 'https://www.empty.webuildwebz.com';
 export const DEFAULT_LANGUAGE = 'es';
-export const HAS_CONFIGURATION = true;
-export const HAS_SESSIONS = true;
+export const HAS_CONFIGURATION = false;
+export const HAS_SESSIONS = false;
