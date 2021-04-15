824be1633fb7312610b8739ef4a3a508e9785e72
lefcott <lefcott@hotmail.com>
2021-04-15 01:48:35 -0300

    LVA-3 added constants

--- src/projects/digital-cards/constants.js
+++ src/projects/digital-cards/constants.js
@@ -1,5 +1,6 @@
 export const PROJECT_CODE = 'digital-cards';
 export const URL = 'https://www.masnegocio.webuildwebz.com';
 export const ALERT_STACK_POSITION = 'bottomRight';
-export const HAS_LOGIN = true;
 export const DEFAULT_LANGUAGE = 'es';
+export const HAS_CONFIGURATION = true;
+export const HAS_SESSIONS = true;
--- src/projects/empty/constants.js
+++ src/projects/empty/constants.js
@@ -1,5 +1,6 @@
 export const PROJECT_CODE = 'empty';
 export const ALERT_STACK_POSITION = 'bottomRight';
 export const URL = 'https://www.empty.webuildwebz.com';
-export const HAS_LOGIN = false;
 export const DEFAULT_LANGUAGE = 'es';
+export const HAS_CONFIGURATION = true;
+export const HAS_SESSIONS = true;
