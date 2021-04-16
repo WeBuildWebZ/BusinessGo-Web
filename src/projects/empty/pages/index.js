da2758f248ac393e59cd9231f33e5464c7bb3bf6
lefcott <lefcott@hotmail.com>
2021-04-16 15:59:49 -0300

    fix empty

--- src/projects/empty/pages/index.js
+++ src/projects/empty/pages/index.js
@@ -1,17 +1,10 @@
-import Banner from "../components/a-banner";
-import Recents from "../components/c-recents";
-import Some from "../components/d-some";
-import One from "../components/f-one";
-import Services from "../components/g-services";
+import EmptyComponent from '../components/EmptyComponent';
 
-export default function Home() {
-  return (
-    <>
-      <Banner />
-      <Recents />
-      <Some />
-      <One />
-      <Services />
-    </>
-  );
-}
+const Empty = () => (
+  <div>
+    Empty project
+    <EmptyComponent />
+  </div>
+);
+
+export default Empty;
