dddc5ad2a8bee547ee559ee133897c84311f008e
lefcott <lefcott@hotmail.com>
2021-04-18 03:41:28 -0300

    hour adjustments

--- src/projects/digital-cards/components/Card2/Design1/BusinessHours/index.js
+++ src/projects/digital-cards/components/Card2/Design1/BusinessHours/index.js
@@ -11,12 +11,30 @@ const BusinessHours = props => {
     <div>
       <div className="businessHoursContainer">
         <div className="businessHours">
-          {card.business_hours?.map((business_hour, i) => (
-            <div key={i} className="businessHour">
-              <div>{business_hour.day}</div>
-              <div className="hour">{business_hour.hour}</div>
-            </div>
-          ))}
+          {Object.keys(card.business_hours || {})
+            .filter(day => card.business_hours[day].enabled)
+            .map((day, i) => {
+              const business_hour = card.business_hours[day];
+              const openingDate = new Date(business_hour.opening_date);
+              const closingDate = new Date(business_hour.closing_date);
+              const openingHours = `${`${openingDate.getHours()}`.padStart(
+                2,
+                '0'
+              )}:${`${openingDate.getMinutes()}`.padStart(2, '0')}`;
+              const closingHours = `${`${closingDate.getHours()}`.padStart(
+                2,
+                '0'
+              )}:${`${closingDate.getMinutes()}`.padStart(2, '0')}`;
+
+              return (
+                <div key={i} className="businessHour">
+                  <div>{language.days[day]}</div>
+                  <div className="hour">
+                    {openingHours}-{closingHours}
+                  </div>
+                </div>
+              );
+            })}
         </div>
         <img className="businessPhoto" src={card.business_photo} />
       </div>
@@ -37,12 +55,14 @@ const BusinessHours = props => {
             display: flex;
             flex-direction: row;
             justify-content: space-between;
+            font-size: 12px;
           }
           .hour {
             margin-left: 7px;
           }
           .businessPhoto {
             width: 50%;
+            height: auto;
           }
         `}
       </style>
