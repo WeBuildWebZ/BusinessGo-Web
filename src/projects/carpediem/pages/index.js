2e6203dd5c12e4c64520306a255679280a4a46ba
josezanle <jose.rios.lm@gmail.com>
2021-04-20 12:28:36 -0300

    cambio de banner

--- src/projects/carpediem/pages/index.js
+++ src/projects/carpediem/pages/index.js
@@ -1,10 +1,10 @@
+import Menu from '../components/menu';
 import Banner from '../components/a-banner';
 import Recents from '../components/c-recents';
 import Some from '../components/d-some';
 import One from '../components/f-one';
 import Services from '../components/g-services';
 import Footer from '../components/h-footer';
-import Menu from '../components/menu';
 
 export default function Home() {
   return (
