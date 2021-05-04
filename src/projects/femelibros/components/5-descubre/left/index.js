47e580a1a36945d47888860ff265492e668583d1
Lefcott <lefcott@hotmail.com>
2021-05-04 20:11:12 -0300

    foos

--- src/projects/femelibros/components/5-descubre/left/index.js
+++ src/projects/femelibros/components/5-descubre/left/index.js
@@ -1,16 +1,28 @@
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
 
-const Left = () => (
-  <div className="left" data-aos="fade-left">
-    <Image src="/images/image1.jpeg" alt="Picture of the author" width={600} height={700} />
-    <style jsx>
-      {`
-        .left {
-          width: 600px;
-        }
-      `}
-    </style>
-  </div>
-);
+const Left = () => {
+  const project = useSelector(store => store.project);
+
+  return (
+    <div className="left" data-aos="fade-left">
+      {project?.configuration.body?.photo1 && (
+        <Image
+          src={project?.configuration.body?.photo1}
+          alt="Picture of the author"
+          width={600}
+          height={700}
+        />
+      )}
+      <style jsx>
+        {`
+          .left {
+            width: 600px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default Left;
