085bc335b42a01bd104365ff6e71b05372a073c8
josezanle <jose.rios.lm@gmail.com>
2021-05-18 13:09:05 -0300

    add circular loader

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -2,71 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Spinner = props => (
-  <div className="spinner" style={props.style}>
-    <div className="subspinner" />
+  <svg style={props.style}>
+    <circle cx="70" cy="70" r="70" />
+
     <style jsx>
       {`
-        .spinner {
-          float: left;
+        svg {
+          position: relative;
           width: 40px;
           height: 40px;
-          border-style: solid;
-          border-width: 1px;
-          animation: main-rotation 1s linear infinite;
+          animation: rotate 2s linear infinite;
         }
 
-        .subspinner {
-          float: left;
-          width: 30%;
-          height: 30%;
-          border-style: solid;
-          border-width: 1px;
-          border-radius: 50%;
-          animation: child-rotation 1s linear infinite;
-        }
-
-        @keyframes main-rotation {
+        @keyframes rotate {
           0% {
-            -ms-transform: rotate(0deg);
-            -moz-transform: rotate(0deg);
-            -webkit-transform: rotate(0deg);
-            -o-transform: rotate(0deg);
             transform: rotate(0deg);
-            border-radius: 0;
-            background-color: rgba(0, 0, 0, 0);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+
+        svg circle {
+          width: 100%;
+          height: 100%;
+          fill: none;
+          stroke-width: 10;
+          stroke: #00a1ff;
+          stroke-linecap: round;
+          transform: translate(5px, 5px);
+          stroke-dashoffset: 440;
+          stroke-dasharray: 440;
+          animation: animate 4s linear infinite;
+        }
+        @keyframes animate {
+          0%,
+          100% {
+            stroke-dashoffset: 440;
           }
           50% {
-            border-radius: 50px;
-            background-color: rgba(0, 0, 0, 0.5);
+            stroke-dashoffset: 0;
           }
-          100% {
-            -ms-transform: rotate(360deg);
-            -moz-transform: rotate(360deg);
-            -webkit-transform: rotate(360deg);
-            -o-transform: rotate(360deg);
-            transform: rotate(360deg);
-          }
-        }
-
-        @keyframes child-rotation {
-          0% {
-            -ms-transform: rotate(360deg);
-            -moz-transform: rotate(360deg);
-            -webkit-transform: rotate(360deg);
-            -o-transform: rotate(360deg);
-            transform: rotate(360deg);
-          }
-          100% {
-            -ms-transform: rotate(0deg);
-            -moz-transform: rotate(0deg);
-            -webkit-transform: rotate(0deg);
-            -o-transform: rotate(0deg);
-            transform: rotate(0deg);
+          50.1% {
+            stroke-dashoffset: 880;
           }
         }
       `}
     </style>
-  </div>
+  </svg>
 );
 
 Spinner.propTypes = {
