8289ab5fb059d45f32d4de3d4d995335176df9b9
Lefcott <lefcott@hotmail.com>
2021-05-31 09:51:01 -0300

    fix

--- src/services/api/itemModel.js
+++ src/services/api/itemModel.js
@@ -1,7 +1,7 @@
 import api from '../../utils/axios';
 
 export const listItemModels = project =>
-  api.get(`api/projects/${encodeURIComponent(project.code)}/item_momdels`);
+  api.get(`api/projects/${encodeURIComponent(project.code)}/item_models`);
 
 export const showItemModel = (project, entity) =>
   api.get(`api/projects/${encodeURIComponent(project.code)}/item_models/${encodeURIComponent(entity)}`);
