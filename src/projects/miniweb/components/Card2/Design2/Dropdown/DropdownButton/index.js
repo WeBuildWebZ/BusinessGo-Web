b569ecdc947ea6356a32bc67b2081a32e83d032f
Lefcott <lefcott@hotmail.com>
2021-06-04 02:22:43 -0300

    basic dropdown content

---
+++ src/projects/miniweb/components/Card2/Design2/Dropdown/DropdownButton/index.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+const DropdownButton = props => (
+  <div className={`dropdownButton${props.open ? ' open' : ''}`} onClick={props.onClick}>
+    <div className="title">{props.title}</div>
+    <style jsx>
+      {`
+        .dropdownButton {
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 50px;
+          margin: 4px;
+          font-weight: bold;
+          background-image: linear-gradient(to top, #000000, #000000 50%, #3e3e3e 100%);
+          box-shadow: 0 0 1px 1px #3e3e3e;
+          border-radius: 20px;
+        }
+        .dropdownButton.open {
+          background-image: linear-gradient(to top, #d8d4d4, #d8d4d4 50%, #f0ecec 100%);
+          color: #383434;
+          border-style: solid;
+          border-width: 5px;
+          border-color: #ffffff;
+          box-shadow: 0 0 1px 1px #ffffff;
+        }
+        .title {
+          margin-left: 21px;
+        }
+      `}
+    </style>
+  </div>
+);
+
+DropdownButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  open: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default DropdownButton;
--- src/projects/miniweb/components/Card2/Design2/Dropdown/index.js
+++ src/projects/miniweb/components/Card2/Design2/Dropdown/index.js
@@ -1,35 +1,21 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-const DropDown = props => {
+import DropdownButton from './DropdownButton';
+
+const Dropdown = props => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className={`dropdown${open ? ' open' : ''}`} onClick={() => setOpen(!open)}>
-      <div className="title">{props.title}</div>
+    <div className={`dropdown${open ? ' open' : ''}`}>
+      <DropdownButton title={props.title} open={open} onClick={() => setOpen(!open)} />
+      {open && <div className="content">{props.children}</div>}
       <style jsx>
         {`
           .dropdown {
-            display: flex;
-            align-items: center;
             width: 90%;
-            height: 50px;
-            margin: 4px;
-            font-weight: bold;
-            background-image: linear-gradient(to top, #000000, #000000 50%, #3e3e3e 100%);
-            box-shadow: 0 0 1px 1px #3e3e3e;
-            border-radius: 20px;
           }
           .dropdown.open {
-            background-image: linear-gradient(to top, #d8d4d4, #d8d4d4 50%, #f0ecec 100%);
-            color: #383434;
-            border-style: solid;
-            border-width: 5px;
-            border-color: #ffffff;
-            box-shadow: 0 0 1px 1px #ffffff;
-          }
-          .title {
-            margin-left: 21px;
           }
         `}
       </style>
@@ -37,9 +23,9 @@ const DropDown = props => {
   );
 };
 
-DropDown.propTypes = {
+Dropdown.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.any
+  children: PropTypes.any.isRequired
 };
 
-export default DropDown;
+export default Dropdown;
