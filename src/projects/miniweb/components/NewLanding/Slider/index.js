622e08c1d972c4f8f298136dc3f2140330083bb4
Lefcott <lefcott@hotmail.com>
2021-06-13 04:09:38 -0300

    icons movement

--- src/projects/miniweb/components/NewLanding/Slider/index.js
+++ src/projects/miniweb/components/NewLanding/Slider/index.js
@@ -5,6 +5,8 @@ const Slider = () => (
     <img className="leftPhone" src={globalThis.getAssetUrl('images/slider/left_phone.png')} />
     <img src={globalThis.getAssetUrl('images/slider/center_phone.png')} />
     <img src={globalThis.getAssetUrl('images/slider/character.png')} />
+    <img className="icons" src={globalThis.getAssetUrl('images/slider/icons.png')} />
+    <img className="icons" src={globalThis.getAssetUrl('images/slider/icons2.png')} />
     <img className="button" src={globalThis.getAssetUrl('images/slider/button.png')} />
     <style jsx>
       {`
@@ -17,6 +19,12 @@ const Slider = () => (
         }
         .dots {
           width: 90%;
+          transform: translate(calc(-50% - 10px), -10px);
+          animation: dotsMovement infinite 4s;
+        }
+        .icons {
+          transform: translate(calc(-50% + 10px), 10px);
+          animation: iconsMovement infinite 3.5s;
         }
         .rightPhone {
           transform: translate(0);
@@ -27,6 +35,24 @@ const Slider = () => (
         .button {
           transform: translate(calc(-50% - 28px), 430px);
           width: 258px;
+        }
+
+        @keyframes dotsMovement {
+          0% {
+            transform: translate(calc(-50% - 10px), -10px);
+          }
+          50% {
+            transform: translate(calc(-50% + 10px), 10px);
+          }
+        }
+
+        @keyframes iconsMovement {
+          0% {
+            transform: translate(calc(-50% + 10px), 10px);
+          }
+          50% {
+            transform: translate(calc(-50% - 10px), -10px);
+          }
         }
       `}
     </style>
