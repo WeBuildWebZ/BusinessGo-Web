f2f569b104ee8f794ffcd411f9ea5c58683eb7a3
Leandro Cotti <lefcott@hotmail.com>
2021-08-30 01:09:22 -0300

    add insertProducts scripts to wow

---
+++ src/projects/blanqueriawow/insertProducts.js
@@ -0,0 +1,28 @@
+(() => {
+  const category = 'Almohadas';
+  const toNumbers = str =>
+    str
+      .split('')
+      .filter(c => c.charCodeAt() >= 48 && c.charCodeAt() <= 57)
+      .join('');
+  const cards = document.getElementsByClassName('card-body');
+  const items = [];
+
+  for (let i = 0; i < cards.length; i += 1) {
+    const card = cards[i];
+    items.push({
+      value: {
+        category,
+        name: card.querySelector('ul li:nth-child(2)').innerHTML,
+        photo: card.querySelector('a img').src,
+        price: +toNumbers(card.querySelector('h1 span').innerHTML),
+        code: toNumbers(card.querySelector('ul li:nth-child(3)').innerHTML),
+        stock: +toNumbers(card.querySelector('ul li:nth-child(4)').innerHTML)
+      },
+      entity: 'product',
+      project_code: 'blanqueriawow_new'
+    });
+  }
+  console.log(`db.items.insertMany(${JSON.stringify(items)});`);
+  console.log(items);
+})();
